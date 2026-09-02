57557e58978e628894abd8d1432ab41e9e4a34e4
Aaron Gil <aaronabdiel09@gmail.com>
2026-09-02 16:39:19 -0400

    add new vents

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,4 +6,36 @@
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+
+
+<nav>
+	<p>PROYECT ARC</p>
+
+	<div>
+		<a href="/">menu</a>
+		<a href="/xbox">xbox</a>
+		<a href="/playstation">playstation</a>
+		<a href="/nintendo">nintendo</a>
+		<a href="/pc">pc</a>
+		
+	</div>
+
+	<div>
+		<span></span>
+		<p>login</p>
+
+	</div>
+
+	<div>
+		<span></span>
+		<p>search</p>
+	</div>
+
+</nav>
+
 {@render children()}
+
+<footer>
+	Este es mi pie de pagina
+</footer>
